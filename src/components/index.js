b4e8e5fba5a6078308f54e7285bbecfe8b6e7e46
WOGHQ <danieljbetancourt4@gmail.com>
2024-03-30 21:50:04 -0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
-import Navbar from "./Navbar.jsx";
+import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
 import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
-import Footer from "/Users/danny/Desktop/WOGHQ/HOOBANK/src/components/Footer.jsx";
+import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 
